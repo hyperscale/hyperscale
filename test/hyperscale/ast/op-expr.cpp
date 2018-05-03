5f8e14727572f10b80db29c408bfe69e59248386
Axel Etcheverry <aetcheverry@molotov.tv>
2018-05-03 14:02:12 +0200

    fix(test): cast int literal to std::size_t

--- test/hyperscale/ast/op-expr.cpp
+++ test/hyperscale/ast/op-expr.cpp
@@ -12,7 +12,6 @@
 #include <boost/test/unit_test.hpp>
 #include <hyperscale/ast/int-expr.hpp>
 #include <hyperscale/ast/op-expr.hpp>
-#include <llvm/ADT/StringRef.h>
 #include <memory>
 
 BOOST_AUTO_TEST_SUITE(hyperscale_ast_op_expr)
@@ -24,13 +23,13 @@ BOOST_AUTO_TEST_CASE(test_op_expr) {
     left.setStartOffset(0);
     left.setLine(1);
     left.setColumn(1);
-    left.setText(llvm::StringRef("12"));
+    left.setText("12");
 
     auto right = hyperscale::parser::Token(hyperscale::syntax::TokenKind::IntegerLiteral);
     right.setStartOffset(5);
     right.setLine(1);
     right.setColumn(6);
-    right.setText(llvm::StringRef("45"));
+    right.setText("45");
 
     auto expr = new hyperscale::ast::OpExpr(
         new hyperscale::ast::IntExpr(left),
