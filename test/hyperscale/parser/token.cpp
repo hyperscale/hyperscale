2b1a6638976ce1ba0633fb319863ffd93e79f571
Axel Etcheverry <axel@etcheverry.biz>
2018-05-03 21:47:57 +0200

    test(parser): Added test for Token

---
+++ test/hyperscale/parser/token.cpp
@@ -0,0 +1,34 @@
+// <hyper/console/command.cpp> -*- C++ -*-
+
+/**
+ * Hyperscale
+ *
+ * (c) 2015-2018 Axel Etcheverry
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+#include <boost/test/unit_test.hpp>
+#include <hyperscale/parser/token.hpp>
+#include <hyperscale/syntax/token_kinds.hpp>
+
+BOOST_AUTO_TEST_SUITE(hyperscale_parser_token)
+
+BOOST_AUTO_TEST_CASE(test_token) {
+    hyperscale::parser::Token tok(hyperscale::syntax::TokenKind::IntegerLiteral);
+
+    tok.setText("1");
+    tok.setColumn(1);
+    tok.setLine(1);
+    tok.setStartOffset(0);
+
+    BOOST_CHECK_EQUAL(tok.getKind(), hyperscale::syntax::TokenKind::IntegerLiteral);
+    BOOST_CHECK_EQUAL(tok.getText(), "1");
+    BOOST_CHECK_EQUAL(tok.getColumn(), std::size_t(1));
+    BOOST_CHECK_EQUAL(tok.getLine(), std::size_t(1));
+    BOOST_CHECK_EQUAL(tok.getStartOffset(), 0);
+    BOOST_CHECK_EQUAL(tok.is(hyperscale::syntax::TokenKind::IntegerLiteral), true);
+    BOOST_CHECK_EQUAL(tok.isNot(hyperscale::syntax::TokenKind::Identifier), true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
